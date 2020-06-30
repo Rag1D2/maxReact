c6f0f8303abda54f6ab2af80350d158eea739e6a
Matt Ragone <m.ragone85@gmail.com>
2020-06-30 09:20:37 -0400

    added binding

--- max-react/src/App.js
+++ max-react/src/App.js
@@ -10,18 +10,45 @@ class App extends Component {
       { name: "Lee", age: 38 },
     ],
   };
+
+  switchNameHandler = (newName, newAge) => {
+    // console.log("Was CLicked!");
+    // DON'T DO THIS: this.state.persons[0].name = "Maximillian";
+    this.setState({
+      persons: [
+        { name: newName, age: 35 },
+        { name: "Michael", age: newAge },
+        { name: "Lee", age: 38 },
+      ],
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 35 },
+        { name: "Michael", age: 23 },
+        { name: "Lee", age: 38 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, "Matthew!!", 36)}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Matt!", 39)}
         >
           My Hobbies: Racing
         </Person>
